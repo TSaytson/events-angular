1e25055b8225906c92d7e93dc86c3618c2803d79
Thiago Saytson <thiagosaytson@poli.ufrj.br>
2025-02-19 04:11:09 -0300

    [feature] filtering by topic or location

---
+++ src/app/events/event.model.ts
@@ -0,0 +1,9 @@
+export interface Event{
+  eventId:number,
+  location: string,
+  eventDate: string,
+  topic: string,
+  qntPeople: number,
+  batch: string,
+  imageUrl: string
+}
--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Event } from './event.model';
 
 @Component({
   selector: 'app-events',
@@ -7,19 +8,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events.component.scss']
 })
 export class EventsComponent implements OnInit {
-
-  public events: any;
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEvents()
   }
 
-  public getEvents(): void {
-    this.http.get('http://localhost:5000/api/events').
-      subscribe(
-        Response => this.events = Response,
-        Error => console.log(Error)
+  public events: Event[] = [];
+  public filteredEvents: Event[] = [];
+  private _topic: string = '';
+  private _location: string = '';
+
+  public get topic(): string {
+    return this._topic;
+  }
+
+  public set topic(value: string) {
+    this._topic = value;
+    this.filteredEvents = this.topic ? this.filterByTopic(this.topic) : this.events;
+  }
+
+  public get location(): string {
+    return this._location;
+  }
+
+  public set location(value: string) {
+    this._location = value;
+    this.filteredEvents = this.location ? this.filterByLocation(this.location) : this.events;
+  }
+
+  filterByTopic(text: string) {
+    text = text.toLocaleLowerCase();
+    if (this.location)
+      return this.events.filter((event) =>
+        event.topic.toLocaleLowerCase().includes(text) &&
+        event.location.toLocaleLowerCase().includes(this.location)
+      )
+    return this.events.filter((event) =>
+      event.topic.toLocaleLowerCase().includes(text)
     )
+  }
+  filterByLocation(text: string) {
+    text = text.toLocaleLowerCase();
+    if (this.topic)
+      return this.events.filter((event) =>
+        event.topic.toLocaleLowerCase().includes(this.topic) &&
+        event.location.toLocaleLowerCase().includes(text)
+      )
+    return this.events.filter((event) =>
+      event.location.toLocaleLowerCase().indexOf(text) !== -1)
+  }
+
+  public getEvents(): void {
+    this.http.get<Event[]>('http://localhost:5000/api/events').
+      subscribe(
+        Response => {
+          this.events = Response
+          this.filteredEvents = this.events;
+        },
+        Error => console.log(Error)
+      )
   }
 }
